459729f04cf5427281566535fad160e0f7fb4eed
Evgeny Saenko <storn84@mail.ru>
2020-02-02 11:24:10 +0300

    changes mode in file"ThreadRoad"

--- src/Lesson05/ThreadRoad.java
+++ src/Lesson05/ThreadRoad.java
@@ -1,29 +1,29 @@
 package Lesson05;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class ThreadRoad{
 
     private static final int size = 10000000;
     private static final int h = size / 2;
-
     private static float[] arr = new float[size];//создали размер массива 10 млн//создали размер массива 10 млн
     private static float[] one = new float[h];  //создадим два массива по 5 млн
     private static float[] two = new float[h];  //создадим два массива по 5 млн
 
     //метод обработки основным одним потоком
     private static void oneThread(){
+        float[] arr = new float[size];//создали размер массива 10 млн//создали размер массива 10 млн
         System.out.println("Началась обработка - 1 потоком");
-        float x = 0;
         for (int i = 0; i <arr.length ; i++) {//заполнили массив единицами
             arr[i] = 1;
         }
         long startTime = System.currentTimeMillis();//стартанули время
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            x = arr[i];
         }
         long stopTime = System.currentTimeMillis();
         float intervalTime = (stopTime - startTime)/1000f;
-        System.out.println("Время обработки одним потоком: "+ intervalTime+" секунд(милисекунд)"+x);
+        System.out.println("Время обработки одним потоком: "+ intervalTime+" секунд(милисекунд)");
     }
 
     //метод обработки двумя потоками
@@ -32,11 +32,8 @@ public class ThreadRoad{
             arr[i] = 1;
         }
         long startTime = System.currentTimeMillis();//стартанули время
-
         System.arraycopy(arr, 0, one, 0, h);  //скопируем содержимое первой половины массива arr в массив one
         System.arraycopy(arr, h, two, 0, h);  //скопируем содержимое второй половины массива arr в массив two
-        System.out.println("Разделили массивы - Потоки начали вычисление");
-
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -49,7 +46,7 @@ public class ThreadRoad{
                 calculationThread(two);
             }
         });
-
+        System.out.println("Разделили массивы - Потоки начали вычисление");
         t1.start();
         t2.start();
 
@@ -59,29 +56,25 @@ public class ThreadRoad{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         System.out.println("Потоки закончили вычисление");
+
         //запоняем обратно массив уже посчитаный в потоках(собираем его)
         System.arraycopy(one, 0, arr, 0, h);
         System.arraycopy(two, 0, arr, h, h);
-
-
         long stopTime = System.currentTimeMillis();
         float intervalTime = (stopTime - startTime)/1000f;
         System.out.println("Время обработки двумя потоками: "+ intervalTime+" секунд(милисекунд)");
     }
 
     //метод который получает массив и считает eго
-    private static synchronized void calculationThread(float [] x){
-        //получает на вход массив и считает его
+    private static void calculationThread(float [] x){
         for (int i = 0; i < x.length; i++) {
             x[i] = (float)(x[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.out.println("закончил");
     }
+
     public static void main(String[] args) {
         oneThread();//этот метод выполняет мейн поток
-
-        twoThread();//запустим метод обработки двумя потоками
+        twoThread();//этот метод запускает мейн, вычисление делают потоки, мейн заканчивает
     }
 }
